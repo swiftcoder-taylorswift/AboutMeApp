8abda4f3663556a971229a973a1ff10b6fbcbe26
Saana Shah <the.real.saana@gmail.com>
2023-07-13 13:57:15 -0700

    Initial Commit

---
+++ AboutMeApp/AboutMeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeAppApp.swift
+//  AboutMeApp
+//
+//  Created by Scholar on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AboutMeApp/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  AboutMeApp
+//
+//  Created by Scholar on 7/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack{
+            Image("background2")
+                .resizable(capInsets: EdgeInsets(top: 1.0, leading: 0.0, bottom: -4.0, trailing: 0.0), resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                .ignoresSafeArea()
+
+                
+                
+                
+            
+                
+                
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
